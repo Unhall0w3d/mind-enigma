0990612bc94e4f97154a2e873a01c65ec7bd6b05
Unhall0w3d <63813991+Unhall0w3d@users.noreply.github.com>
2020-04-23 16:40:44 -0400

    Script update.
    Update script to collect multiple IPs and run loop against n IP addresses.

--- consoleLogs_subprocessMethod.py
+++ consoleLogs_subprocessMethod.py
@@ -2,18 +2,25 @@
 import subprocess
 
 #Define Variables
-ipAddress = input('IP Address: ')
+x = input('How many phones do we need logs for?: ')
+x = int(x)
+ipAddress = []
 destFolder = input('Folder Location to Store Files - It Must Exist (e.g. /home/sampson/): ')
 
-#Inform user what we're doing
-print('Beginning download of Phone Console logs from ' + ipAddress)
+
+#Collect IP Addresses
+for i in range(x) :
+        ipAddress.append(input('What is the phone IP address?: '))
 
 #Download files from the Serviceability page. This includes /FS/ directory storing console logs. 
-subprocess.call ('wget -r --accept "messages*, *.tar.gz" http://' + ipAddress + '/CGI/Java/Serviceability?adapter=device.statistics.consolelog' + ' -P ' + destFolder, shell=True)
+for n in ipAddress :
+        subprocess.call ('wget -T 5 --tries=2 -r --accept "messages*, *.tar.gz" http://' + n + '/CGI/Java/Serviceability?adapter=device.statistics.consolelog' + ' -P ' + destFolder, shell=True)
+#Below line is commented out. Download of the status messages or "plain text" on the webpage was not working. Will utilize another mechanism as spawning subprocesses to run bash tasks isn't what Python is for.
+#       subprocess.call ('curl --max-time 5 -s http://' + n + '/CGI/Java/Serviceability?adapter=device.settings.status.messages -o ~/StatusMessages' + n + '.txt', shell=True)
 
 #Inform user download is complete, indicate where files are stored.
 print('#################################################################################')
 print('#################################################################################')
-print('Files have been stored in ' + destFolder + 'in a folder named ' + ipAddress + '.')
+print('Files have been stored in ' + destFolder + ' in an IP specific folder.')
 print('#################################################################################')
 print('#################################################################################')
