e9a9c2234e154e54351965edc28171b94e32c71a
Unhall0w3d <63813991+Unhall0w3d@users.noreply.github.com>
2020-04-23 09:09:12 -0700

    Add files via upload

---
+++ consoleLogs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import subprocess
+
+#Define Variables
+ipAddress = input('IP Address: ')
+destFolder = input('Folder Location to Store Files - It Must Exist (e.g. /home/sampson/): ')
+
+#Inform user what we're doing
+print('Beginning download of Phone Console logs from ' + ipAddress)
+
+#Download files from the Serviceability page. This includes /FS/ directory storing console logs. 
+subprocess.call ('wget -r --accept "messages*, *.tar.gz" http://' + ipAddress + '/CGI/Java/Serviceability?adapter=device.statistics.consolelog' + ' -P ' + destFolder, shell=True)
+
+#Inform user download is complete, indicate where files are stored.
+print('Files have been stored in ' + destFolder + 'in a folder named ' + ipAddress + '.')
