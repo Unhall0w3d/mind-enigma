10e1d34fe72f409c4fed3ee82f76ca436e5df741
Unhall0w3d <63813991+Unhall0w3d@users.noreply.github.com>
2020-04-25 10:57:33 -0400

    Update Script
    Add *.log to accepted files to download.

--- consoleLogs_subprocessMethod.py
+++ consoleLogs_subprocessMethod.py
@@ -14,9 +14,7 @@ for i in range(x) :
 
 #Download files from the Serviceability page. This includes /FS/ directory storing console logs. 
 for n in ipAddress :
-        subprocess.call ('wget -T 5 --tries=2 -r --accept "messages*, *.tar.gz" http://' + n + '/CGI/Java/Serviceability?adapter=device.statistics.consolelog' + ' -P ' + destFolder, shell=True)
-#Below line is commented out. Download of the status messages or "plain text" on the webpage was not working. Will utilize another mechanism as spawning subprocesses to run bash tasks isn't what Python is for.
-#       subprocess.call ('curl --max-time 5 -s http://' + n + '/CGI/Java/Serviceability?adapter=device.settings.status.messages -o ~/StatusMessages' + n + '.txt', shell=True)
+        subprocess.call ('wget -T 5 --tries=2 -r --accept "*.log, messages*, *.tar.gz" http://' + n + '/CGI/Java/Serviceability?adapter=device.statistics.consolelog' + ' -P ' + destFolder, shell=True)
 
 #Inform user download is complete, indicate where files are stored.
 print('#################################################################################')
