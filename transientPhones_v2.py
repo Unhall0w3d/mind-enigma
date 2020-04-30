4762e260140ca2bea7f73d7659aac94ecf9dccf4
Unhall0w3d <63813991+Unhall0w3d@users.noreply.github.com>
2020-04-30 11:01:53 -0400

    Creation of TransientPhones v2.
    Uses BeautifulSoup html parsing and requests rather than subprocess.

---
+++ transientPhones_v2.py
@@ -0,0 +1,29 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+
+#Define how many phones we need to hit
+x = input('How many phones?: ')
+x = int(x)
+
+#Collect IP addresses in tuple
+ipAddress = []
+
+#Here we loop to grab the list of IP Addresses to access.
+for i in range(x):
+        ipAddress.append(input('What is the phone IP address?: '))
+
+#Here we loop to access each IP address provided (equivalent of Network Configuration page) to collect Device Type + MAC + Registered state.
+for n in ipAddress:
+        URL = 'http://' + n + '/CGI/Java/Serviceability?adapter=device.statistics.configuration' #URL is dynamically created based on IPs collected
+        page = requests.get(URL, timeout=6)
+        soup = BeautifulSoup(page.content, 'html.parser')
+#looking for instance of SEP* or CIPC*, such as CIPCKPERRY or SEPAABBCCDDEEFF. Returned as variable 'results'
+        results = soup.find(text=re.compile('SEP*|CIPC*'))
+#looking for instance of "Active" on the webpage indicating device is registered to a given CCM. Returned as variable 'results2'
+        results2 = soup.find_all(text=re.compile('Active'))
+#conditional statement that dictates if "Active" is not found, indicate the phone is not registered. Otherwise report the device it is registered to. (e.g. cucmpub.ipt.local Active)
+        if results2 is None:
+                print(results, + " is not registered to CCM.")
+        else:
+                print(results, results2)
