7fdc4ab2690e2125c71739557936eef81df53ae9
Kenneth Perry <kenoperry@gmail.com>
2020-05-03 00:30:47 -0400

    Changes to be commited: 	Error handling for socket.timeout exception if WebGUI does not respond.
    Modified:
    	transientPhones_v2.py

--- transientPhones_v2.py
+++ transientPhones_v2.py
@@ -13,24 +13,30 @@ from bs4 import BeautifulSoup
 x = input('How many phones?: ')
 x = int(x)
 
-#Collect IP addresses in tuple
+#Collect IP addresses in tuple (list)
 ipAddress = []
 
 #Here we loop to grab the list of IP Addresses to access.
 for i in range(x):
-        ipAddress.append(input('What is the phone IP address?: '))
+	ipAddress.append(input('What is the phone IP address?: '))
 
 #Here we loop to access each IP address provided (equivalent of Network Configuration page) to collect Device Type + MAC + Registered state.
 for n in ipAddress:
-        URL = 'http://' + n + '/CGI/Java/Serviceability?adapter=device.statistics.configuration' #URL is dynamically created based on IPs collected
-        page = requests.get(URL, timeout=6)
-        soup = BeautifulSoup(page.content, 'html.parser')
+	try:
+		global results
+		global results2
+		URL = 'http://' + n + '/CGI/Java/Serviceability?adapter=device.statistics.configuration' #URL is dynamically created based on IPs collected
+		page = requests.get(URL, timeout=6)
+		soup = BeautifulSoup(page.content, 'html.parser')
 #looking for instance of SEP* or CIPC*, such as CIPCKPERRY or SEPAABBCCDDEEFF. Returned as variable 'results'
-        results = soup.find(text=re.compile('SEP*|CIPC*'))
+		results = soup.find(text=re.compile('SEP*|CIPC*'))
 #looking for instance of "Active" on the webpage indicating device is registered to a given CCM. Returned as variable 'results2'
-        results2 = soup.find_all(text=re.compile('Active'))
+		results2 = soup.find_all(text=re.compile('Active'))
 #conditional statement that dictates if "Active" is not found, report only the device model and name. Otherwise report the device it is registered to. (e.g. cucmpub.ipt.local Active)
-        if results2 is None:
-                print(results)
-        else:
-                print(results, results2)
+	except:
+		print('Connection to ' + n + ' timed out. Trying next.')
+
+if results2 is None:
+	print(results)
+else:
+	print(results, results2)
