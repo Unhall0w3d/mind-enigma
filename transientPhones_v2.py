09c057931dd58063c42c957ed06cafe30d18985d
Unhall0w3d <63813991+Unhall0w3d@users.noreply.github.com>
2020-05-05 18:41:45 -0400

    Rewrite of script into functions
    Rewrite of script into functions

--- transientPhones_v2.py
+++ transientPhones_v2.py
@@ -6,38 +6,35 @@
 # Juliana Xu                        #
 #####################################
 
-import re
-import requests
+import re, requests
 from bs4 import BeautifulSoup
 
-#Define how many phones we need to hit
-x = input('How many phones?: ')
-x = int(x)
-
-#Collect IP addresses in tuple (list)
-ipAddress = []
-
-#Here we loop to grab the list of IP Addresses to access.
-for i in range(x):
-	ipAddress.append(input('What is the phone IP address?: '))
+def phoneCollection():
+        x = input('How many phones?: ')
+        x = int(x)
+        global ipAddress
+        ipAddress = []
+        for i in range(x):
+                ipAddress.append(input('What is the phone IP address?: '))
 
 #Here we loop to access each IP address provided (equivalent of Network Configuration page) to collect Device Type + MAC + Registered state.
-for n in ipAddress:
-	try:
-		global results
-		global results2
-		URL = 'http://' + n + '/CGI/Java/Serviceability?adapter=device.statistics.configuration' #URL is dynamically created based on IPs collected
-		page = requests.get(URL, timeout=6)
-		soup = BeautifulSoup(page.content, 'html.parser')
+def webScrape():
+        try:
+                URL = 'http://' + n + '/CGI/Java/Serviceability?adapter=device.statistics.configuration' #URL is dynamically created based on IPs collected
+                page = requests.get(URL, timeout=6)
+                soup = BeautifulSoup(page.content, 'html.parser')
 #looking for instance of SEP* or CIPC*, such as CIPCKPERRY or SEPAABBCCDDEEFF. Returned as variable 'results'
-		results = soup.find(text=re.compile('SEP*|CIPC*'))
+                results = soup.find(text=re.compile('SEP*|CIPC*'))
 #looking for instance of "Active" on the webpage indicating device is registered to a given CCM. Returned as variable 'results2'
-		results2 = soup.find_all(text=re.compile('Active'))
-#conditional statement that dictates if "Active" is not found, report only the device model and name. Otherwise report the device it is registered to. (e.g. cucmpub.ipt.local Active)
-	except:
-		print('Connection to ' + n + ' timed out. Trying next.')
+                results2 = soup.find_all(text=re.compile('Active'))
+                print(results, results2)
+#conditional statement that dictates if "Active" is not found, report only the device model and name. Otherwise report the device it is registered to. (e.g. cucmpub.ipt>
+        except:
+                print('Connection to ' + n + ' timed out. Trying next.')
 
-if results2 is None:
-	print(results)
-else:
-	print(results, results2)
+#Run collection for how many phones we will connect to, as well as the IP Addresses.
+phoneCollection()
+
+#Now loop for each appended IP Address and run webScrape function
+for n in ipAddress:
+        webScrape()
