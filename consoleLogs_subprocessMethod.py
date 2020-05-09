1ec8da38cac7a1b020c8465b3cc6b32685c8204b
Unhall0w3d <kenoperry@gmail.com>
2020-05-09 11:55:30 -0400

    Update script based on lessons learned from other projects. Code cleanup/reformat. Change destination directory from user defined to ~/ (/home/<usr>/) to minimize user input.

--- consoleLogs_subprocessMethod.py
+++ consoleLogs_subprocessMethod.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python
 import subprocess
 
-#Define Variables
-x = input('How many phones do we need logs for?: ')
-x = int(x)
+# Define Variables
+x = int(input('How many phones do we need logs for?: '))
 ipAddress = []
-destFolder = input('Folder Location to Store Files - It Must Exist (e.g. /home/sampson/): ')
+destFolder = str('~/')
 
+# Collect IP Addresses
+for i in range(x):
+    ipAddress.append(input('What is the phone IP address?: '))
 
-#Collect IP Addresses
-for i in range(x) :
-        ipAddress.append(input('What is the phone IP address?: '))
+# Download files from the Serviceability page. This includes /FS/ directory storing console logs.
+for n in ipAddress:
+    subprocess.call(
+        'wget -T 5 --tries=2 -r --accept "*.log, messages*, *.tar.gz" http://' + n + '/CGI/Java/Serviceability?adapter=device.statistics.consolelog' + ' -P ' + destFolder,
+        shell=True)
 
-#Download files from the Serviceability page. This includes /FS/ directory storing console logs. 
-for n in ipAddress :
-        subprocess.call ('wget -T 5 --tries=2 -r --accept "*.log, messages*, *.tar.gz" http://' + n + '/CGI/Java/Serviceability?adapter=device.statistics.consolelog' + ' -P ' + destFolder, shell=True)
-
-#Inform user download is complete, indicate where files are stored.
+# Inform user download is complete, indicate where files are stored.
 print('#################################################################################')
 print('#################################################################################')
 print('Files have been stored in ' + destFolder + ' in an IP specific folder.')
