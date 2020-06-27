331b45f1bcd6ef5133a83e7095f4e061e782c6f9
UnHall0w3d <kenoperry@gmail.com>
2020-06-27 14:12:14 -0400

    condensed script, supplement password collection with getpass, add nodename identification, note why sip trunks report multiple times, add blank print statements for output formatting.

--- ccmRisPortRegCheck.py
+++ ccmRisPortRegCheck.py
@@ -16,6 +16,7 @@ from requests import Session
 from requests.auth import HTTPBasicAuth
 from lxml import etree
 import urllib3
+from getpass import getpass
 
 # Disable insecure warning
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -25,7 +26,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 def infocollect():
     ipaddr = input("What is the CUCM IP?: ")
     username = input("What is the CUCM Username?: ")
-    password = input("What is the CUCM Password?: ")
+    password = getpass("What is the CUCM Password?: ")
     return ipaddr, username, password
 
 
@@ -43,56 +44,48 @@ def inputfetch():
 # Phone and SIP Trunk as we cannot pull Dir Number.
 def regcheck(ccmip, ccmun, ccmpw):
     devtype = input("Are we looking for Phones or SIP Trunks? If both, select trunk. (phone|trunk): ")
+    # Define WSDL location at http url, we do not store it locally
+    macs = inputfetch()
+    print()
+    print('Collecting Data...')
+    print()
+    wsdl = 'https://' + ccmip + ':8443/realtimeservice2/services/RISService70?wsdl'
+    session = Session()
+    session.verify = False
+    session.auth = HTTPBasicAuth(ccmun, ccmpw)
+    transport = Transport(cache=SqliteCache(), session=session, timeout=20)
+    history = HistoryPlugin()
+    client = Client(wsdl=wsdl, transport=transport, plugins=[history])
+    factory = client.type_factory('ns0')
+    item = []
+    for mac in macs:
+        item.append(factory.SelectItem(Item=mac))
+    devnames = factory.ArrayOfSelectItem(item)
+    stateinfo = ''
     if devtype == "phone":
-        # Define WSDL location at http url, we do not store it locally
-        macs = inputfetch()
-        wsdl = 'https://' + ccmip + ':8443/realtimeservice2/services/RISService70?wsdl'
-        session = Session()
-        session.verify = False
-        session.auth = HTTPBasicAuth(ccmun, ccmpw)
-        transport = Transport(cache=SqliteCache(), session=session, timeout=20)
-        history = HistoryPlugin()
-        client = Client(wsdl=wsdl, transport=transport, plugins=[history])
-        factory = client.type_factory('ns0')
-        item = []
-        for mac in macs:
-            item.append(factory.SelectItem(Item=mac))
-        devnames = factory.ArrayOfSelectItem(item)
-        stateinfo = ''
         criteria = factory.CmSelectionCriteria(
             MaxReturnedDevices=1000, DeviceClass='Phone', Model=255, Status='Any', NodeName='', SelectBy='Name',
             SelectItems=devnames, Protocol='Any', DownloadStatus='Any')
         result = client.service.selectCmDevice(stateinfo, criteria)
         for node in result.SelectCmDeviceResult.CmNodes.item:
+            nodename = node.Name
             for device in node.CmDevices.item:
-                print("Device Name: " + device.Name, "Status: " + device.Status,
+                print("Node: " + nodename, "Device Name: " + device.Name, "Status: " + device.Status,
                       "Description: " + device.Description, "Dir Number: " + device.DirNumber)
-    elif devtype == "trunk":
-        # Define WSDL location at http url, we do not store it locally
-        macs = inputfetch()
-        wsdl = 'https://' + ccmip + ':8443/realtimeservice2/services/RISService70?wsdl'
-        session = Session()
-        session.verify = False
-        session.auth = HTTPBasicAuth(ccmun, ccmpw)
-        transport = Transport(cache=SqliteCache(), session=session, timeout=20)
-        history = HistoryPlugin()
-        client = Client(wsdl=wsdl, transport=transport, plugins=[history])
-        factory = client.type_factory('ns0')
-        item = []
-        for mac in macs:
-            item.append(factory.SelectItem(Item=mac))
-        devnames = factory.ArrayOfSelectItem(item)
-        stateinfo = ''
+                print()
+    if devtype == "trunk":
+        print("SIP Trunks that are enabled to Run On All Nodes will report per node below.")
+        print()
         criteria = factory.CmSelectionCriteria(
             MaxReturnedDevices=1000, DeviceClass='Any', Model=255, Status='Any', NodeName='', SelectBy='Name',
             SelectItems=devnames, Protocol='Any', DownloadStatus='Any')
         result = client.service.selectCmDevice(stateinfo, criteria)
         for node in result.SelectCmDeviceResult.CmNodes.item:
+            nodename = node.Name
             for device in node.CmDevices.item:
-                print("SIP Trunks that appear multiple times due to assignment to multiple RG/RL will appear multiple"
-                      "times in the below output.")
-                print("Device Name: " + device.Name, "Status: " + device.Status,
+                print("Node: " + nodename, "Device Name: " + device.Name, "Status: " + device.Status,
                       "Description: " + device.Description)
+                print()
     checkhistory = input("Do you want to check the SOAP Message History?(y/n): ")
     if checkhistory == "y":
         for hist in [history.last_sent, history.last_received]:
