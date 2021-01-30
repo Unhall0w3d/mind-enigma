d7a2f58f7eddc60ce0541dd761562db7b6a4991d
kenop <kenoperry@gmail.com>
2021-01-29 19:05:27 -0500

    Lots of changes. Reference existing vars for file names (filename, timestampedfn) and directory (path) instead of static paths. Added instructional commments. Removed print statements, see readme for how this should be run. Fixed verbiage on error messages in log file. Added notification of exception hit in print statement. Added (and commented out) statements to email using "mail" linux application, email out on exception hit (script failing), and made the script loop, sleeping 12 hours between loops.

--- expresswayMemoryCheck.py
+++ expresswayMemoryCheck.py
@@ -2,6 +2,8 @@
 # Script created by Ken Perry
 # Script purpose is to verify memory util on Expressway Servers
 # Script method is SSH to pull output of 'cat /proc/meminfo | grep Committed_AS'
+# Script is set to run every 12 hours to provide feedback.
+# If you need this script to check your expressway memory in this manner, you likely need to do it 1-2 times daily.
 #
 
 import time
@@ -9,7 +11,10 @@ import paramiko
 import os
 from getpass import getpass
 
-# Define list of IP Addresses to SSH to
+# Define list of IP Addresses to SSH to.
+# Replace the ip-addr# in quotes with an IP address (e.g. 10.161.1.133), one per line.
+# Follow the syntax below to add additional entries
+
 hostname = [
     "ip-addr1",
     "ip-addr2"
@@ -20,14 +25,14 @@ timestr = time.strftime("%Y%m%d-%H%M%S")
 dirname = 'temp'
 dir_path = os.getcwd()
 path = os.path.join(dir_path, dirname)
-filename = 'ExpresswayHC' + timestr + '.txt'
-tarname = 'ExpresswayHC' + timestr + '.tar.gz'
+filename = 'ExpresswayHC.txt'
+timestampedfn = 'ExpresswayHC' + timestr + '.txt'
 
 # Check if desired directory exists, create it otherwise
 if os.path.exists(dirname) is False:
     os.mkdir(dirname)
 
-# Input Requirements taken from user inputs
+# Input Requirements taken from Env Variables
 username = input("Username: ")
 password = getpass("Password: ")
 
@@ -35,10 +40,8 @@ password = getpass("Password: ")
 # Command to execute
 command = "cat /proc/meminfo | grep Committed_AS"
 
-# Initialize SSH Client
+# Initialize SSH Client & Add to Known Hosts
 client = paramiko.SSHClient()
-
-# Add to Known Hosts
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 # For each ip address in our list, SSH to the device.
@@ -46,42 +49,39 @@ client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 # One text file per script run, script checks all required devices.
 
 
-def healthcheck(filename, hostname):
+def healthcheck():
     for ipaddr in hostname:
         try:
             client.connect(hostname=ipaddr, username=username, password=password)
-            print("+" * 10, ipaddr, "+" * 10)
-            print("=" * 10, command, "=" * 10)
             stdin, stdout, stderr = client.exec_command(command)
-            print(stdout.read().decode())
             err = stderr.read().decode()
-            if err:
-                print(err)
-            with open(os.path.join(filename), 'a+') as rdr:
-                rdr.write("+" * 10 + ipaddr + "+" * 10 + '\r')
-                rdr.write("=" * 10 + command + "=" * 10 + '\r')
+            with open(os.path.join(path, filename), 'a+') as rdr:
+                rdr.write("+" * 5 + ipaddr + "+" * 5 + '\r')
+                rdr.write(command + '\r')
                 rdr.write(stdout.read().decode() + '\r')
                 if err:
                     rdr.write(stderr.read().decode() + '\r')
         except:
-            print("[!] Cannot connect to the Expressway Server " + ipaddr + ". Please manually verify access.")
-            with open(os.path.join(filename), 'a+') as rdr:
-                rdr.write('[!] Cannot connect to the Expressway Server ' + ipaddr + '. Please manually verify access.' + '\r')
+            with open(os.path.join(path, filename), 'a+') as rdr:
+                rdr.write('[!] Cannot connect to ' + ipaddr + '. Please verify reachability & Credentials.' + '\r')
 
 
-def filehandling(filename, tarname):
-    os.system('tar jcvf ' + tarname + ' ' + filename)
-    os.system('rm ' + filename)
-    os.system('mv ' + tarname + ' ' + path)
+def filehandling():
+    # The below allows you to email the file to your email, or a distro list if you have smtp configured on your linux machine.
+    # "mail" command should work before trying to use it through this script.
+    # os.system('mail -s "Expressway Healthchecks" email@domain.com < ' + path + filename + '')
+    os.system('cp ' + path + filename + ' ' + path + timestampedfn + '')
+    os.system('rm ' + path + filename)
 
 
-Flag = True
-while Flag == True:
+while True:
     try:
-        healthcheck(filename, hostname)
-        filehandling(filename, tarname)
-        print("Script ran successfully, data was tarballed and stored in //current/working/directory/temp/")
-        exit()
-    except KeyboardInterrupt:
-        print("\r\n")
-        exit()
+        healthcheck()
+        filehandling()
+        # Uncomment the line below if you use the email function above.
+        # print("Email has been sent.")
+    except:
+        print("Oh no! We failed somewhere. We'll try again in 6 hours. Use Email function for better notifications.")
+        # os.system('mail -s "Script Stopped" email@domain.com')
+    finally:
+        time.sleep(43200)
