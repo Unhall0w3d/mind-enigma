4057dde02373bbbda767197f25d1ca90f97eb452
Ken Perry <kenoperry@gmail.com>
2022-12-16 16:35:51 -0500

    Draft script created by ChatGPT with minor additions

--- WIP_Rework/zeepTraceDownloader.py
+++ WIP_Rework/zeepTraceDownloader.py
@@ -17,35 +17,20 @@ from getpass import getpass
 import time
 
 # Define current time.
-timestr = time.strftime("%Y%m%d-%H%M%S")
+now = time.strftime("%Y%m%d-%H%M%S")
 
-def infocollect():
-    # Define user input required for script; pub ip, username, pw.
-    cucmipaddr = str(input('What is the target UC Server Pub IP?: '))
-    cucmusername = str(input('What is the GUI Username?: '))
-    cucmpassword = getpass('What is the GUI Password?: ')
-    try:
-        print('Checking AXL Interface Availability.')
-        r = requests.get('https://' + cucmipaddr + '/axl', auth=HTTPBasicAuth(cucmusername, cucmpassword), verify=False)
-        if r.status_code == 401:
-            print('AXL Interface is unreachable. HTTP 401 Unauthorized Received.')
-            print('Please ensure the AXL URL is reachable at https://<ucm-ip>/axl.')
-            print('Ensure the credentials and version info is correct.')
-            print('Script Exiting.')
-            exit()
-        elif r.status_code == 200:
-            print('AXL Interface is available. HTTP 200 received.')
-            return cucmipaddr, cucmversion, cucmpassword, cucmusername
-    except Exception as e:
-        print(e)
-        exit()
+# Prompt the user for their CUCM username and password
+ipaddr = input("Enter your CUCM Publisher IP Address: ")
+print('Supported Versions: 12.5 | 12.0 | 11.5 | 11.0 | 10.5 | 10.0 | 9.1 | 9.0')
+ver = input("Enter your CUCM Version: ")
+username = input("Enter your CUCM GUI Username: ")
+password = getpass("Enter your CUCM GUI Password: ")
 
+# Set up the AXL API client
+wsdl = 'https://' + ipaddr + '/axl/service/v12.0/AXLAPI.wsdl'
+client = Client(wsdl=wsdl, wsse=UsernameToken(username, password))
 
-def tracednld():
-    # Set up the AXL API client
-    wsdl = 'https://' + cucmipaddr + '/axl/service/v12.0/AXLAPI.wsdl'
-    client = Client(wsdl=wsdl, wsse=UsernameToken(cucmusername, cucmpassword))
-
+while True:
     # Make the listTraceFile request
     response = client.service.listTraceFile(name='trace-file-name', fileFormat='zip')
 
@@ -54,21 +39,21 @@ def tracednld():
         print(f"{trace_file['name']} ({trace_file['id']})")
 
     # Prompt the user to select a trace file
-    selected_trace_id = input("Enter the ID of the trace file you want to download: ")
+    selected_trace_id = input("Enter the ID of the trace file you want to download (or 'q' to quit): ")
+
+    if selected_trace_id.lower() == 'q':
+        # Exit the loop if the user enters 'q'
+        break
 
     # Make the getTraceFile request using the selected trace file ID
     response = client.service.getTraceFile(id=selected_trace_id, fileFormat='zip')
 
     # Save the trace file data to a local file
-    with open(cucmipaddr + '_' + selected_trace_id + '_' + timestr + '.zip', 'wb') as f:
+    with open(now + '_' + ipaddr + '_' + selected_trace_id + '.zip', 'wb') as f:
         f.write(base64.b64decode(response['return']['traceFile']))
 
-
-if __name__ == "__main__":
-    try:
-        infocollect()
-        tracednld()
-        exit()
-    except Exception as x:
-        print(x)
-        exit()
+    # Prompt the user to download another trace file
+    download_more = input("Do you want to download another trace file? (y/n) ")
+    if download_more.lower() != 'y':
+        # Exit the loop if the user does not want to download another trace file
+        break
