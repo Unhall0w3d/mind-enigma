e956f9b2afff4304c63073229ce8bd88750562af
Ken Perry <kenoperry@gmail.com>
2022-12-29 14:27:30 -0500

    remove old test scripts, add esxi test script

---
+++ WIP_Rework/vmEsxiTest.py
@@ -0,0 +1,110 @@
+# Import the necessary libraries
+import os
+import paramiko
+from pyVim.connect import SmartConnect, Disconnect
+from pyVmomi import vim
+
+
+def list_vms():
+    # Connect to the vSphere server
+    print("Enter the vSphere server hostname:")
+    server = input()
+    print("Enter the vSphere server username:")
+    username = input()
+    print("Enter the vSphere server password:")
+    password = input()
+    si = SmartConnect(host=server, user=username, pwd=password)
+
+    # Retrieve a list of all virtual machines in the vSphere environment
+    vm_view = si.content.viewManager.CreateContainerView(si.content.rootFolder, [vim.VirtualMachine], True)
+    vm_list = vm_view.view
+    vm_view.Destroy()
+
+    # Print information about each virtual machine
+    for vm in vm_list:
+        print(f"Name: {vm.name}")
+        print(f"Description: {vm.summary.config.annotation}")
+        print(f".vmx file location: {vm.summary.config.vmPathName}")
+        if vm.summary.guest is not None:
+            print(f"VMware Tools version: {vm.summary.guest.toolsVersion}")
+        print()
+        # Disconnect from the vSphere server
+    Disconnect(si)
+
+
+def list_files():
+    # Connect to the remote system using SCP or SFTP
+    print("Enter the protocol to use (scp or sftp):")
+    protocol = input()
+    print("Enter the hostname of the remote system:")
+    hostname = input()
+    print("Enter the username to use to connect to the remote system:")
+    hostuser = input()
+    print("Enter the password to use to connect to the remote system:")
+    hostpassword = input()
+    print("Enter the directory on the remote system to list files from:")
+    directory = input()
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname, username=hostuser, password=hostpassword)
+    if protocol == 'scp':
+        scp = paramiko.SFTPClient.from_transport(client.get_transport())
+    elif protocol == 'sftp':
+        sftp = client.open_sftp()
+
+    # List the files in the specified directory
+    file_list = []
+    if protocol == 'scp':
+        file_list = scp.listdir(directory)
+    elif protocol == 'sftp':
+        file_list = sftp.listdir(directory)
+
+    # Print the list of files
+    print("Files:")
+    for i, file in enumerate(file_list):
+        print(f"{i + 1}. {file}")
+    print()
+
+    # Prompt the user to select the files to download
+    while True:
+        print("Enter the numbers of the files to download, separated by spaces (or enter 'q' to quit):")
+        choices = input()
+        if choices == 'q':
+            # Parse the user's choices
+            try:
+                choices = [int(c) for c in choices.split()]
+            except ValueError:
+                print("Invalid input. Please try again.")
+                continue
+
+            # Download the selected files
+            print("Enter the local directory to download files to:")
+            destination = input()
+            for i, file in enumerate(file_list):
+                if i + 1 in choices:
+                    if protocol == 'scp':
+                        scp.get(os.path.join(directory, file), local_path=destination)
+                    elif protocol == 'sftp':
+                        sftp.get(os.path.join(directory, file), local_path=destination)
+                    print(f"{file} downloaded.")
+            print()
+
+        while True:
+            # Display the menu
+            print("Menu:")
+            print("1. List virtual machines")
+            print("2. List and download files")
+            print("3. Quit")
+
+            # Prompt the user to select an option
+            print("Enter your choice:")
+            choice = input()
+
+            # Perform the selected option
+            if choice == '1':
+                list_vms()
+            elif choice == '2':
+                list_files()
+            elif choice == '3':
+                break
