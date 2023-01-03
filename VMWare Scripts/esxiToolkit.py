8afe2db8637eb6f10179c1d59c5d71dd40303a30
Ken Perry <kenoperry@gmail.com>
2023-01-02 20:47:06 -0500

    move snapshots to api only, move maintenance mode to api only, added vmtool check for vm power option

--- VMWare Scripts/esxiToolkit.py
+++ VMWare Scripts/esxiToolkit.py
@@ -115,12 +115,29 @@ class ESXi:
         vm_list = vm_view.view
         vm_view.Destroy()
 
+        host_view = self.si.content.viewManager.CreateContainerView(self.si.content.rootFolder, [vim.HostSystem], True)
+        host_list = host_view.view
+        host_view.Destroy()
+
+        # Find the host that we want to manage
+        for h in host_list:
+            host = h
+            break
+        print(host.runtime.healthSystemRuntime.hardwareStatusInfo.memoryStatusInfo)
+        print(host.runtime.healthSystemRuntime.hardwareStatusInfo.cpuStatusInfo)
+        print(host.runtime.healthSystemRuntime.hardwareStatusInfo.storageStatusInfo)
+        exit()
         # Print information about each virtual machine
         print(f"Performing data pull for device {self.server}. Please wait...")
         with open(os.path.join(os.path.expanduser('~'),
                                'Downloads/' + self.server + '_' + timestr + '_' + 'VMDetails.txt'),
                   'w+') as f:
             for vm in vm_list:
+                f.write(f"--- ESXi Report for {host.name} ---\n")
+                f.write(f"Power state: {host.runtime.powerState}\n")
+                f.write(f"Connection State: {host.runtime.connectionState}\n")
+                f.write(f"Maintenance Mode: {host.runtime.inMaintenanceMode}\n")
+                f.write(f"Boot Time: {host.runtime.bootTime}\n")
                 f.write(f"--- VM Report for {vm.name} ---\n")
                 f.write(f"Name: {vm.name}\n")
                 f.write(f"Power state: {vm.summary.runtime.powerState}\n")
@@ -290,11 +307,23 @@ class ESXi:
             selected_vm_index = int(input("Enter the number of the virtual machine to snapshot: ")) - 1
             selected_vm = vm_list.view[selected_vm_index]
 
+            # Collect snapshot name, description, memory dump, and fs quiesce
+            name = input("Please enter a name for the snapshot: ")
+            description = input("Please provide a description for the snapshot: ")
+            memory = input("Do you want to include a memory dump in the snapshot? (y/N): ")
+            if memory == "y":
+                memory = True
+            else:
+                memory = False
+            quiesce = input("Do you want to enable file system quiesce? (y/N): ")
+            if quiesce == "y":
+                quiesce = True
+            else:
+                quiesce = False
             # Perform a snapshot on the selected virtual machine
-            snapshot_task = selected_vm.CreateSnapshot_Task(name="Snapshot", description="Snapshot of VM", memory=False,
-                                                            quiesce=False)
-            snapshot_task.wait()
-            print(f"Snapshot complete for Virtual Machine {selected_vm}!")
+            snapshot_task = selected_vm.CreateSnapshot_Task(name=name, description=description, memory=memory,
+                                                            quiesce=quiesce)
+            print(f"Snapshot complete for Virtual Machine {selected_vm.name}!")
 
             doagain = input("Do you want to take another snapshot? (y/N)").lower()
             if doagain == 'y':
@@ -345,143 +374,146 @@ class ESXi:
                                                             + timestr + '_' + 'configBundle.tgz'), 'wb+') as f:
                 f.write(r.content)
 
-    # Function to perform a VM snapshot using CLI/SSH
-    def clisnapshot(self):
-        # Gather VMs and IDs
-        stdin, stdout, stderr = self.ssh.exec_command("vim-cmd vmsvc/getallvms")
-        vms = stdout.readlines()
-        for vm in vms:
-            print(vm)
+    def vmpower(self):
+        vm_view = self.si.content.viewManager.CreateContainerView(self.si.content.rootFolder,
+                                                                  [vim.VirtualMachine],
+                                                                  True)
+        vm_list = vm_view.view
+        vm_view.Destroy()
+        # Get a list of all the VMs on the host
+        vms = self.si.content.rootFolder.childEntity[0].vmFolder.childEntity
 
+        # Print the name and power state of each VM
+        vm_dict = {}
+        vmid = []
+        print(f"Reporting VMs on {self.server}.")
+        print(f"-------------------------------")
+        for i, vm in enumerate(vms):
+            vm_dict[i] = vm
+            print(f"{i} - {vm.summary.config.name}: {vm.summary.runtime.powerState}")
+            if vm.summary.guest.toolsRunningStatus is not None:
+                runstate = vm.summary.guest.toolsRunningStatus
+                if runstate != "guestToolsRunning":
+                    print(f"Virtual Machine {vm.summary.config.name} is showing a VMTools status of {runstate}.")
+                    print(f"As such, the script cannot manipulate the power state on VM {vm.summary.config.name}.")
+                    vmid.append(i)
         while True:
-            # Prompt the user to select a VM
-            vm_id = input("Enter the ID of the VM to snapshot: ")
-            snapshot_name = input("Enter the name of the VM snapshot: ")
-            verify = input(f"Are you sure you want to snapshot VM {vm_id}? (y/n): ").lower()
-            if verify == 'y':
-                # Run snapshot against relevant VM
-                cmd = "vim-cmd vmsvc/snapshot.create %s %s" % (vm_id, snapshot_name)
-                stdin, stdout, stderr = self.ssh.exec_command(cmd)
-                print(stdout.read().decode('utf-8'))
-                verify = input("Would you like to perform another snapshot? (y/n): ").lower()
-                if verify == 'y':
+            try:
+                # Prompt the user to select a VM
+                print(vmid)
+                selection = int(input("Select a VM by number, or (q)uit: "))
+                if selection in vmid:
+                    print(f"You cannot perform a power action on {vm.summary.config.name}.")
                     continue
                 else:
-                    break
-            else:
+                    vm = vm_dict[selection]
+                    # Get the current power state of the VM
+                    power_state = vm.summary.runtime.powerState
+                    print(f"The power state for selection {selection} is {power_state}.")
+                    if power_state == "poweredOn":
+                        verify = input(f"Do you want to proceed with powering off VM {selection}? (y/n): ").lower()
+                        if verify == "y":
+                            # Power off the VM
+                            vm.PowerOff()
+                            print(f"VM {selection} powered off")
+                            print("Giving time for task to complete... standby...")
+                            time.sleep(60)
+                            print("Verifying VM power states...")
+                            # Get a list of all the VMs on the host
+                            vms = self.si.content.rootFolder.childEntity[0].vmFolder.childEntity
+
+                            # Print the name and power state of each VM
+                            vm_dict = {}
+                            print(f"Reporting VMs on {self.server}.")
+                            print(f"-------------------------------")
+                            for i, vm in enumerate(vms):
+                                summary = vm.summary
+                                vm_dict[i] = vm
+                                print(f"{i} - {summary.config.name}: {summary.runtime.powerState}")
+                            print("--------------------------------")
+                            print("Please verify the VM Power State matches the desired state.")
+                            print("Some VMs take longer than others, check the vSphere Web Page if concerned.")
+                            print("Returning to menu...")
+                            time.sleep(5)
+                            break
+                        else:
+                            print("Returning to menu...")
+                            break
+                    else:
+                        verify = input(f"Do you want to proceed with powering on VM {selection}? (y/n): ").lower()
+                        if verify == "y":
+                            # Power on the VM
+                            vm.PowerOn()
+                            print(f"VM {selection} powered on.")
+                            print("Giving time for task to complete... standby...")
+                            time.sleep(60)
+                            print("Verifying VM power states...")
+                            # Get a list of all the VMs on the host
+                            vms = self.si.content.rootFolder.childEntity[0].vmFolder.childEntity
+
+                            # Print the name and power state of each VM
+                            vm_dict = {}
+                            print(f"Reporting VMs on {self.server}.")
+                            print(f"-------------------------------")
+                            for i, vm in enumerate(vms):
+                                summary = vm.summary
+                                vm_dict[i] = vm
+                                print(f"{i} - {summary.config.name}: {summary.runtime.powerState}")
+                            print("--------------------------------")
+                            print("Please verify the VM Power State matches the desired state.")
+                            print("Some VMs take longer than others, check the vSphere Web Page if concerned.")
+                            print("Returning to menu...")
+                            time.sleep(5)
+                            break
+                        else:
+                            break
+            except ValueError:
+                print("Returning to menu...")
+                time.sleep(1)
                 break
-
-    def getvms(self):
-        # Execute the "vim-cmd vmsvc/getallvms" command to get a list of all virtual machines and their power state
-        stdin, stdout, stderr = self.ssh.exec_command("vim-cmd vmsvc/getallvms")
-
-        # Read the command output
-        output = stdout.readlines()
-
-        # Create a list to store the virtual machines and their power state
-        vm_list = []
-
-        # Parse the command output
-        for line in output:
-
-            # Split the line into fields
-            fields = line.split()
-
-            # Check if the line contains a virtual machine
-            if len(fields) > 0:
-                # Get the virtual machine ID and name
-                vm_id = fields[0]
-                vm_name = fields[1]
-
-                # Get the power state of the virtual machine
-                stdin, stdout, stderr = self.ssh.exec_command("vim-cmd vmsvc/power.getstate {}".format(vm_id))
-                power_state = stdout.read().strip()
-                power_state = power_state.decode('utf-8')
-                power_state = power_state[23:]
-
-                # Add the virtual machine and its power state to the list
-                vm_list.append((vm_id, vm_name, power_state))
-        return vm_list
-
-    def vmchoice(self, vm_list):
-        vm_list.pop(0)
-        print(f"Current VM Status on {self.server}:")
-        print("------------------------------------")
-        for vm in vm_list:
-            vm_id, vm_name, power_state = vm
-            print("ID: [{}] Name: [{}] Power state: [{}]".format(vm_id, vm_name, power_state))
-        while True:
-            onoroff = input("Do you want to power a VM ON, or OFF? (ON/OFF): ").lower()
-            print()
-            if onoroff == "on":
-                id = input("Enter the ID of the virtual machine you want to power on: ")
-                proceed = input(f"Are you sure you want to power on VM {id}? (y/n): ").lower()
-                if proceed == "n":
-                    print("Exiting. Please relaunch the script if needed.")
-                    exit()
-                else:
-                    return id, onoroff
-            elif onoroff == "off":
-                id = input("Enter the ID of the virtual machine you want to power off: ")
-                proceed = input(f"Are you sure you want to power off VM {id}? (y/n): ").lower()
-                if proceed == "n":
-                    print("Exiting. Please relaunch the script if needed.")
-                    exit()
-                else:
-                    return id, onoroff
-
-    # Function to power off a virtual machine gracefully
-    def power_onoff_vm(self, id, onoroff):
-        if onoroff == "on":
-            # Execute the "vim-cmd vmsvc/power.shutdown" command to power on the virtual machine gracefully
-            stdin, stdout, stderr = self.ssh.exec_command("vim-cmd vmsvc/power.on {}".format(id))
-
-            # Read the command output
-            output = stdout.readlines()
-            print(f"{self.server} response: {output[0]}")
-            print()
-            print(f"Power On issued for VM {id} on {self.server}.")
-            print("Checking VM power states to verify success...")
-            time.sleep(30)
-
-        elif onoroff == "off":
-            # Execute the "vim-cmd vmsvc/power.shutdown" command to power off the virtual machine gracefully
-            stdin, stdout, stderr = self.ssh.exec_command("vim-cmd vmsvc/power.shutdown {}".format(id))
-            print(f"Power Off issued for VM {id} on {self.server}.")
-            print("Checking VM power states to verify success...")
-            time.sleep(60)
-
-    # Quick function to verify VM power state
-    def vmpowercheck(self, vm_list):
-        print(f"Current VM Status on {self.server}:")
-        for vm in vm_list:
-            vm_id, vm_name, power_state = vm
-            print("ID: {} Name: {} Power state: {}".format(vm_id, vm_name, power_state))
-        print()
-        print("Please verify that the reported power state (above) matches expected VM power state.")
-        print("For any issues, such as VM remaining ON after Power Down, please check the Admin Webpage. ")
-        print("Returning to menu...")
-        time.sleep(5)
 
     # Function to check status of maint mode, enable/disable, and recheck status
     def maintmode(self):
-        # Run command to check maintenance mode status
-        stdin, stdout, stderr = self.ssh.exec_command("esxcli system maintenanceMode get")
-        status = stdout.readlines()[0].strip()
+        vm_view = self.si.content.viewManager.CreateContainerView(self.si.content.rootFolder, [vim.HostSystem], True)
+        vm_list = vm_view.view
+        vm_view.Destroy()
 
-        # Print maintenance mode status and prompt user to enable or disable
-        if "Enabled" in status:
-            print("Maintenance mode is currently enabled")
-            action = input("Do you want to disable maintenance mode? (y/n) ")
-            if action == "y":
-                self.ssh.exec_command("esxcli system maintenanceMode set --enable=false")
-                print("Maintenance mode disabled")
-        elif "Disabled" in status:
-            print("Maintenance mode is currently disabled")
-            action = input("Do you want to enable maintenance mode? (y/n) ")
-            if action == "y":
-                self.ssh.exec_command("esxcli system maintenanceMode set --enable=true")
-                print("Maintenance mode enabled")
+        # Find the host that we want to manage
+        for h in vm_list:
+            host = h
+            break
+
+        # Check the current state of Maintenance Mode
+        if host.runtime.inMaintenanceMode:
+            print(f"Maintenance Mode is currently enabled on {self.server}.")
+            opt = input("Do you want to disable Maintenance Mode? (y/n): ")
+            if opt == 'y':
+                opt = input(f"Are you sure you want to disable Maintenance Mode on {self.server}? (y/n): ")
+                if opt == 'y':
+                    host.ExitMaintenanceMode(timeout=0)
+                    print(f"Exiting Maintenance Mode on {self.server}")
+                    time.sleep(2)
+                else:
+                    print(f"Maintenance Mode will remain enabled on {self.server}")
+                    time.sleep(1)
+            else:
+                print(f"Maintenance Mode will remain enabled on {self.server}")
+                time.sleep(1)
+        else:
+            print(f"Maintenance Mode is currently disabled on {self.server}")
+            opt = input("Do you want to enable Maintenance Mode? (y/n): ")
+            if opt == 'y':
+                opt = input(f"Are you sure you want to enable Maintenance Mode on {self.server}? (y/n): ")
+                if opt == 'y':
+                    host.EnterMaintenanceMode(timeout=0)
+                    print(f"Entering Maintenance Mode on {self.server}")
+                    time.sleep(2)
+                else:
+                    print(f"Maintenance Mode will remain disabled {self.server}")
+                    time.sleep(1)
+            else:
+                print(f"Maintenance Mode will remain disabled {self.server}")
+                time.sleep(1)
 
 
 # Run the main function
@@ -496,12 +528,11 @@ if __name__ == "__main__":
         print("1. List VMs & Details")
         print("2. List And Download Log Files")
         print("3. Collect ESXi HealthCheck")
-        print("4. Perform a VM Snapshot via API (Testing)")
-        print("5. Perform a VM Snapshot via CLI")
-        print("6. Perform an ESXi Config Backup")
-        print("7. Power Off/On VM")
-        print("8. Enable/Disable Maintenance Mode")
-        print("9. Quit")
+        print("4. Perform a VM Snapshot")
+        print("5. Perform an ESXi Config Backup")
+        print("6. Power Off/On VM")
+        print("7. Enable/Disable Maintenance Mode")
+        print("8. Quit")
         print("-----------------")
         print()
         choice = input("Choice: ")
@@ -516,19 +547,12 @@ if __name__ == "__main__":
         elif choice == '4':
             ESXi.vmsnapshot()
         elif choice == '5':
-            ESXi.clisnapshot()
-        elif choice == '6':
             ESXi.configbackup()
+        elif choice == '6':
+            ESXi.vmpower()
         elif choice == '7':
-            print("Run Menu Opt #1. Check output to confirm VM Tools is installed before proceeding.")
-            print("If using open-vm-tools, output will show guestToolsUnmanaged. Proceed.")
-            vm_list = ESXi.getvms()
-            id, onoroff = ESXi.vmchoice(vm_list)
-            ESXi.power_onoff_vm(id, onoroff)
-            ESXi.vmpowercheck(vm_list)
-        elif choice == '8':
             ESXi.maintmode()
-        elif choice == '9':
+        elif choice == '8':
             ESXi.ssh.close()
             Disconnect(ESXi.si)
             exit()
