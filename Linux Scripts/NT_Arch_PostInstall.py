0a9d345065c36a06ca06fb33c9b367e12fb17ecc
Kenneth Perry <kenoperry@gmail.com>
2023-05-01 13:28:28 -0400

    update script to handle kernel install for liquorix, zen, default. Add desktop environment install for most major DEs. Add display manager selection.

--- Linux Scripts/NT_Arch_PostInstall.py
+++ Linux Scripts/NT_Arch_PostInstall.py
@@ -1,4 +1,5 @@
 import subprocess
+import sys
 
 
 def run_command(command):
@@ -119,6 +120,8 @@ def install_display_manager():
         command = "sudo pacman -S lightdm lightdm-gtk-greeter --needed"
         return_code = run_command(command)
         print(f"\nCommand exited with return code: {return_code}")
+        print("Select LightDM Greeter:")
+        install_greeter()
     elif choice == '3':
         command = "sudo pacman -S lxdm --needed"
         return_code = run_command(command)
@@ -172,6 +175,66 @@ def install_desktop_environment():
         install_desktop_environment()
 
 
+def install_yay():
+    print("Building and Installing Yay...")
+    command = "sudo pacman -S --needed git base-devel"
+    return_code = run_command(command)
+    command = "git clone https://aur.archlinux.org/yay.git"
+    return_code = run_command(command)
+    command = "cd yay && makepkg -si"
+    return_code = run_command(command)
+    print(f"\nCommand exited with return code: {return_code}")
+
+
+def install_aur_helper():
+    print("Select AUR Helper:")
+    print("1. Yay")
+    print("2. Return to Main Menu")
+
+    choice = input("Enter your choice (1-2): ")
+
+    if choice == '1':
+        install_yay()
+    elif choice == '2':
+        return
+    else:
+        print("Invalid choice. Please try again.")
+        install_aur_helper()
+
+
+def install_greeter():
+    print("Select Greeter:")
+    print("1. lightdm-gtk-greeter")
+    print("2. lightdm-slick-greeter")
+    print("3. lightdm-webkit-theme-litarvan")
+    print("4. lightdm-mini-greeter")
+    print("5. Return to Main Menu")
+
+    choice = input("Enter your choice (1-5): ")
+
+    if choice == '1':
+        command = "sudo pacman -S lightdm-gtk-greeter --needed"
+        return_code = run_command(command)
+        print(f"\nCommand exited with return code: {return_code}")
+    elif choice == '2':
+        command = "yay -S lightdm-slick-greeter --needed"
+        return_code = run_command(command)
+        print(f"\nCommand exited with return code: {return_code}")
+    elif choice == '3':
+        command = "sudo pacman -S lightdm-webkit-theme-litarvan --needed"
+        return_code = run_command(command)
+        print(f"\nCommand exited with return code: {return_code}")
+    elif choice == '4':
+        command = "sudo pacman -S lightdm-mini-greeter --needed"
+        return_code = run_command(command)
+        print(f"\nCommand exited with return code: {return_code}")
+    elif choice == '5':
+        return
+    else:
+        print("Invalid choice. Please try again.")
+        install_greeter()
+
+
 def submenu():
     print("Select Kernel:")
     print("1. Install Optimized Liquorix Kernel")
@@ -203,9 +266,10 @@ def menu():
     print("1. Install Kernel")
     print("2. Install Desktop Environment")
     print("3. Install Display Manager")
-    print("4. Exit")
+    print("4. Install AUR Helper")
+    print("5. Exit")
 
-    choice = input("Enter your choice (1-4): ")
+    choice = input("Enter your choice (1-5): ")
 
     if choice == '1':
         submenu()
@@ -214,10 +278,14 @@ def menu():
     elif choice == '3':
         install_display_manager()
     elif choice == '4':
-        print("Exiting...")
+        install_aur_helper()
+    elif choice == '5':
+        print("Exiting script...")
+        sys.exit()
     else:
         print("Invalid choice. Please try again.")
         menu()
 
 
-menu()
+if __name__ == "__main__":
+    menu()
